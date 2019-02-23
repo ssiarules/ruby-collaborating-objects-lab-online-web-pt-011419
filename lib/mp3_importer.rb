972a02c945091a8ad90713520de49a8dcd148ee7
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 18:49:43 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,14 +9,11 @@ class MP3Importer
     @path = path 
   end 
   
-  def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-  end
   
-  # def files
-  #   #only wants .mps files 
-  #   Dir.entries(path).select {|entry| entry.include?(".mp3")}
-  # end 
+  def files
+    #only wants .mps files 
+    Dir.entries(path).select {|entry| entry.include?(".mp3")}
+  end 
   
   def import
     #new files arrays put it through the song class method new by file name
