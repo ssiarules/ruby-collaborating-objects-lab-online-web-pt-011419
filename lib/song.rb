1dc7f6f22c9ce2d2b27e0cd0c981c19af1371cb8
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 17:47:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,11 +10,13 @@ class Song
     artist= file.split ("-")[0]
     song= self.new (song_name)
     song.artist_name= artist 
+    song
     
   end
   
-  def artist_name
-      self.artist.name
+  def artist_name=(name)
+      self.artist= Artist.find_or_create_by_name(name)
+      self.artist.add_song(self)
     end
    
   
