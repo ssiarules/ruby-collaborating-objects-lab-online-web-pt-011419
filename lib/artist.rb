7968296b0053b8a0d2ddcf433a1987c040eab78a
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 18:51:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,9 +23,20 @@ class Artist
     @@all
   end 
   
-  def self.find_or_create_by_name(name)
-    self.all.find {|artist| artist.name == name} || self.new(name).save
-  end 
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+  
+  # def self.find_or_create_by_name(name)
+  #   self.all.find {|artist| artist.name == name} || self.new(name).save
+  # end 
   
   def print_songs 
     @songs.each do |song|
