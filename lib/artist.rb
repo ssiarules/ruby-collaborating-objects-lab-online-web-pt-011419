50652a019c7ca386095aaa7070e1c9027f383e22
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 18:38:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-    self.all.find{|artist| artist.name == name || self.new(name).save
+    self.all.find {|artist| artist.name == name} || self.new(name).save
   end 
   
   def print_songs 
